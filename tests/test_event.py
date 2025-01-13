65b6b9c767b16241499bbb00074f316cc3bcdd5d
Bora Yarkın <31701197+bora-yarkin@users.noreply.github.com>
2025-01-13 23:56:44 +0300

    Remove empty tests

--- tests/test_event.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from app import create_app, db
-from app.models import User, Event
-
-
-class EventTestCase(unittest.TestCase):
-    # Similar setup and teardown methods
-    # Write tests for event functionalities
-    pass
